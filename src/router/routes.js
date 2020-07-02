0f47850cf49b4dbd1ca3fdd50e65b47adc7e18c3
rosfiarani <48117151+rosfiarani@users.noreply.github.com>
2020-07-02 09:45:07 +0700

    second commit

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,39 +5,63 @@ import { Notify } from "quasar";
 
 // cek auth apakah sudah login atau belum
 const auth = function(to, from, next) {
-    let isLoggedIn = store().getters["Auth/isLoggedIn"];
-    if (isLoggedIn) {
-        next();
-    } else {
-        next("/login");
-    }
+  let isLoggedIn = store().getters["Auth/isLoggedIn"];
+  if (isLoggedIn) {
+    next();
+  } else {
+    next("/login");
+  }
 };
 
+const routes = [
+  {
+    path: "/",
+    beforeEnter: multiguard([auth]),
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("pages/Home.vue")
+      },
+      {
+        path: "/detail_reward",
+        name: "detail_reward",
+        component: () => import("pages/Detail_reward")
+      },
+      {
+        path: "account",
+        component: () => import("pages/Account.vue")
+      },
+      {
+        path: "profile",
+        component: () => import("pages/Profile.vue")
+      },
+      {
+        path: "password",
+        component: () => import("pages/Password.vue")
+      },
+      {
+        path: "account_settings",
+        component: () => import("pages/Account_settings.vue")
+      },
+      {
+        path: "edit",
+        component: () => import("pages/Edit.vue")
+      }
+    ]
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("pages/Login")
+  },
 
-const routes = [{
-        path: '/',
-        beforeEnter: multiguard([auth]),
-        component: () =>
-            import ('layouts/MainLayout.vue'),
-        children: [{
-            path: '',
-            component: () =>
-                import ('pages/Home.vue')
-        }],
-    }, {
-        path: '/login',
-        name: 'login',
-        component: () =>
-            import ('pages/Login')
-    },
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: "*",
+    component: () => import("pages/Error404.vue")
+  }
+];
 
-    // Always leave this as last one,
-    // but you can also remove it
-    {
-        path: '*',
-        component: () =>
-            import ('pages/Error404.vue')
-    }
-]
-
-export default routes
+export default routes;
